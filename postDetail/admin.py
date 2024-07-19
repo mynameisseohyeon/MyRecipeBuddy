31efe01beda3bf4e064b2da9a4400fb361314159
seohyeon <uimi3606@gmail.com>
2024-07-19 15:45:55 +0900

    ✨ django 서버에서 PostContent에 작성한 title과 content 가져오기 성공!

--- postDetail/admin.py
+++ postDetail/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import PostContent
 
-# Register your models here.
+admin.site.register(PostContent)
--- postDetail/views.py
+++ postDetail/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from .models import MainContent
+from .models import PostContent
 
 def postDetailPage(request):
-    post_list = MainContent.objects.order_by('-pub_date')
-    return render(request, 'pages/postDetailPage.html', {'post_list': post_list})
+    post_list = PostContent.objects.order_by('-pub_date')
+    post_content = {'post_list': post_list}
+
+    return render(request, 'pages/postDetailPage.html', post_content)
