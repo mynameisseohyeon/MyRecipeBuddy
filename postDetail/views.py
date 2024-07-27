922310f28c95fb20fbd5f445b69e4a94614e213e
seohyeon <uimi3606@gmail.com>
2024-07-27 17:15:00 +0900

    🐛 서버에서 댓글 받아오기 성공!

--- postDetail/views.py
+++ postDetail/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import PostContent
 from .forms import CommentForm 
+from django.contrib.auth.decorators import login_required 
 
 def postDetailPage(request):
     post_list = PostContent.objects.order_by('-pub_date')
@@ -8,7 +9,8 @@ def postDetailPage(request):
 
     return render(request, 'pages/postDetailPage.html', post_content)
 
-def comment_create(request, content_id): 
+@login_required(login_url='postDetail:login') #비로그인 상태에서 댓글 작성 시 로그인 화면으로 이동
+def comment_create(request, content_id): #댓글 기능
     content_list = get_object_or_404(PostContent, pk=content_id) 
  
     if request.method == 'POST': 
