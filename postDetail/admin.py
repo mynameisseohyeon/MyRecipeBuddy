c59dce978a20c5f48291a188a9476a76c175fad8
seohyeon <uimi3606@gmail.com>
2024-07-27 17:52:29 +0900

    ✨ 관리자 페이지 성능 개선

--- postDetail/admin.py
+++ postDetail/admin.py
@@ -1,5 +1,14 @@
 from django.contrib import admin
 from .models import PostContent, Comment
 
-admin.site.register(PostContent)
-admin.site.register(Comment)
+# 관리자 페이지 개선 (가독성이 좋도록 수정)
+class PostContentAdmin(admin.ModelAdmin): 
+    list_display = ['title', 'content', 'pub_date'] 
+    search_fields = ['title'] 
+
+class CommentAdmin(admin.ModelAdmin): 
+    list_display = ['content_list', 'content', 'author', 'create_date', 'modify_date'] 
+    search_fields = ['author'] 
+
+admin.site.register(PostContent, PostContentAdmin)
+admin.site.register(Comment, CommentAdmin)
